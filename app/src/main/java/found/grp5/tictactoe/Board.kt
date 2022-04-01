f151bef3766b5c4cfcac45dc81419a4683adf50f
SaudKadiri <saudkadiri@gmail.com>
2022-04-01 22:03:32 +0530

    Basic transition

---
+++ app/src/main/java/found/grp5/tictactoe/Board.kt
@@ -0,0 +1,11 @@
+package found.grp5.tictactoe
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Board : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_board)
+    }
+}
---
+++ app/src/main/java/found/grp5/tictactoe/MainActivity.kt
@@ -0,0 +1,25 @@
+package found.grp5.tictactoe
+
+import android.R.attr
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.R.attr.button
+import android.content.Intent
+import android.view.View
+import android.widget.Toast
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val multiplayer_btn: Button = findViewById(R.id.multiplayer_btn);
+        multiplayer_btn.setOnClickListener {
+            //Toast.makeText(this@MainActivity, "You clicked me.", Toast.LENGTH_SHORT).show()
+            val intent = Intent(this, Board::class.java)
+            startActivity(intent)
+        }
+    }
+}
