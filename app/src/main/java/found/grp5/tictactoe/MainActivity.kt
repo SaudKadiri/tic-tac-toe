690bab7418f64947c78553d502d92894a7cabe2d
SaudKadiri <saudkadiri@gmail.com>
2022-04-01 22:14:33 +0530

    Removed unused imports

--- app/src/main/java/found/grp5/tictactoe/MainActivity.kt
+++ app/src/main/java/found/grp5/tictactoe/MainActivity.kt
@@ -1,13 +1,11 @@
 package found.grp5.tictactoe
 
-import android.R.attr
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.R.attr.button
 import android.content.Intent
-import android.view.View
-import android.widget.Toast
+
+//import android.widget.Toast
 
 
 class MainActivity : AppCompatActivity() {
@@ -15,14 +13,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val multiplayerBtn: Button = findViewById(R.id.multiplayer_btn);
+        val multiplayerBtn: Button = findViewById(R.id.multiplayer_btn)
         multiplayerBtn.setOnClickListener {
-            //Toast.makeText(this@MainActivity, "You clicked me.", Toast.LENGTH_SHORT).show()
+//            Toast.makeText(this@MainActivity, "You clicked me.", Toast.LENGTH_SHORT).show()
             val intent = Intent(this, Board::class.java)
             startActivity(intent)
         }
 
-        val vsComputerBtn: Button = findViewById(R.id.vs_comp_btn);
+        val vsComputerBtn: Button = findViewById(R.id.vs_comp_btn)
         vsComputerBtn.setOnClickListener {
             val intent = Intent(this, Board::class.java)
             startActivity(intent)
